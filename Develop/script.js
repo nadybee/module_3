74db1fb28230921ecc45898f2233f1f7e00be75d
Nadybee <natalie@yoodlize.com>
2022-05-01 08:21:11 -0600

    added commenting

--- Develop/script.js
+++ Develop/script.js
@@ -2,11 +2,10 @@
 const generateBtn = document.querySelector("#generate");
 const slider = document.getElementById("my-range");
 const output = document.getElementById("value");
-const chooseLower = document.getElementById('lowercase')
-const chooseUpper = document.getElementById('uppercase')
-const chooseSpecial = document.getElementById ('special')
-const chooseNumber =document.getElementById('number')
-
+const chooseLower = document.getElementById("lowercase");
+const chooseUpper = document.getElementById("uppercase");
+const chooseSpecial = document.getElementById("special");
+const chooseNumber = document.getElementById("number");
 
 /** Arrays needed for password */
 let password = [];
@@ -43,31 +42,39 @@ const alphaUpper = alphabet.map((letter) => {
 });
 const specialArr = ["!", "@", "#", "%", "?", "&"];
 const numArr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-const allCharArr = [...alphabet, ...alphaUpper, ...specialArr, ...numArr];
 const arrOfAll = [alphabet, alphaUpper, specialArr, numArr];
 
-// const noUpper = [...alphabet, ...specialArr, ...numArr]
-// const noSpecial = [...alphabet, ...alphaUpper, ...numArr]
-// const noNum= [...alphabet, ...alphaUpper,...specialArr]
+/**-------------FUNCTIONS---------------------*/
 
-/** function to identify what the user wants */
-let userChoice = []
-const createUserChoiceArr = () =>{
-if (chooseLower.checked) {
-  userChoice.push(0)
-}
-if (chooseUpper.checked){
-  userChoice.push(1)
-}
-if (chooseSpecial.checked){
-  userChoice.push(2)
-}
-if (chooseNumber.checked){
-  userChoice.push(3)
-}
-return userChoice
-}
+/** function to create array of user critera selections */
+let userChoice = [];
+const createUserChoiceArr = () => {
+  if (chooseLower.checked) {
+    userChoice.push(0);
+  }
+  if (chooseUpper.checked) {
+    userChoice.push(1);
+  }
+  if (chooseSpecial.checked) {
+    userChoice.push(2);
+  }
+  if (chooseNumber.checked) {
+    userChoice.push(3);
+  }
+  return userChoice;
+};
 
+/** create main array from user selection to choose password characters */
+
+let arrForPassword = [];
+
+const pushToArrForPassword = () => {
+  createUserChoiceArr();
+  for (i = 0; i < userChoice.length; i++) {
+    arrForPassword.push(arrOfAll[userChoice[i]]);
+  }
+  return arrForPassword.flat();
+};
 /** Function to get random index from an Array */
 const getRandom = (arr) => {
   return arr[Math.floor(Math.random() * arr.length)];
@@ -83,78 +90,52 @@ const shuffleArray = (array) => {
   }
 };
 
-/** create main array for for random characters */
-
-let arrForPassword = []
-
-const pushToArrForPassword = () => {
-  createUserChoiceArr()
-  for (i=0 ; i<userChoice.length; i++){
-   arrForPassword.push(arrOfAll[userChoice[i]])
-  } 
-  return arrForPassword.flat()
- }
-
-/** function to generate minimum 8 char password with all char */
-const passwordGenerate8All = () => {
-
-  if (chooseLower.checked){
-    
+/** function to generate minimum character requirement for user selections */
+const passwordGenerateAll = () => {
+  if (chooseLower.checked) {
     password.push(getRandom(alphabet));
   }
-  if (chooseUpper.checked){
-    password.push (getRandom(alphabet).toUpperCase())
+  if (chooseUpper.checked) {
+    password.push(getRandom(alphabet).toUpperCase());
   }
-  if (chooseSpecial.checked){
-    password.push (getRandom(specialArr))
+  if (chooseSpecial.checked) {
+    password.push(getRandom(specialArr));
   }
-  if (chooseNumber.checked){
-    password.push (getRandom(numArr))
+  if (chooseNumber.checked) {
+    password.push(getRandom(numArr));
   }
-
   return password;
 };
 
 /** fill remains spots in the password array */
-
 const remainingPasswordAll = () => {
-passwordGenerate8All()
+  passwordGenerateAll();
   while (password.length < slider.value) {
     password.push(getRandom(pushToArrForPassword()));
   }
-  
   return password;
 };
 
-
+/** Generates final password and shuffles it */
 const generatePassword = () => {
-remainingPasswordAll()
+  remainingPasswordAll();
 
   shuffleArray(password);
 
   return password;
- 
 };
 
-// generatePassword()
-console.log(password.join(""));
-console.log(password.length);
+/** --------User Selection functionality ------------ */
 
-
-// /** slider functionality */
-
-
-/** advanced selectors for dom */
-
+// /** slider to choose password length */
 
 output.innerHTML = slider.value;
-
 slider.oninput = function () {
   output.innerHTML = this.value;
   slider.addEventListener("input", function () {
-    let x = slider.value *.78;
-    var color =`linear-gradient(90deg, hsl(360, 91%, 36%) ${x}%, lightgray ${x}%)`;
-      slider.style.background=color;
+    let x = slider.value * 0.78;
+    var color = `linear-gradient(90deg, hsl(360, 91%, 36%) ${x}%, lightgray ${x}%)`;
+    slider.style.background = color;
   });
 };
 
@@ -163,22 +144,9 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password.join("");
-  // passwordLength = parseInt(window.prompt('length of password'))
-
 }
 
-/** function to add or remove checked status on radio button */
-
-function uncheck(button){
-  if (button.checked) {
-    button.checked = false
-  }
-
-}
 
 // // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-/** Event listeners to check and uncheck selections */
-chooseLower.addEventListener ('change',uncheck(chooseLower))
 
